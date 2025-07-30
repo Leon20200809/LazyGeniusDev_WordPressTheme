2ea4c327906df1c312f31e15e56521ba8f06cb59
Leon.C <kindlyhearted666@gmail.com>
2025-07-30 10:01:50 +0900

    copyright追記

--- src/LazyGeniusDev_WordPressTheme/footer.php
+++ src/LazyGeniusDev_WordPressTheme/footer.php
@@ -1,7 +1,9 @@
     <footer class="site-footer">
         <div class="container">
-            <p>&copy; <?= date('Y'); ?> <em>Automate through laziness, accelerate through impatience, safeguard quality through pride.</em><br>
-    ―「怠惰で仕組み化し、短気で高速化し、傲慢で品質を守ります。」</p>
+            <p>&copy; <?= date('Y'); ?> LazyGenius™. All rights reserved.<br>
+                <em>Automate through laziness, accelerate through impatience, safeguard quality through pride.</em><br>
+                ―「怠惰で仕組み化し、短気で高速化し、傲慢で品質を守ります。」
+            </p>
         </div>
     </footer>
     
